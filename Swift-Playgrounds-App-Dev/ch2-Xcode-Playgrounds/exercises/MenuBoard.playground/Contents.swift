5f658142c62a6a4e1763dc7ee303d8080d933f1b
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-23 20:20:25 -0700

    Add title to view

--- Swift-Playgrounds-App-Dev/ch2-Xcode-Playgrounds/exercises/MenuBoard.playground/Contents.swift
+++ Swift-Playgrounds-App-Dev/ch2-Xcode-Playgrounds/exercises/MenuBoard.playground/Contents.swift
@@ -6,9 +6,25 @@ import UIKit
 import PlaygroundSupport
 
 class MenuBoard : UIViewController {
+    
+    var textView = UITextView()
+    var menuText = "Company\n"
+    
+    func addViews() {
+        textView = UITextView(frame: view.frame)
+        textView.font = UIFont(name: "Chalkduster", size: 22.0)
+        textView.textColor = UIColor.yellow
+        textView.backgroundColor = UIColor(white: 0.25, alpha: 0.9)
+        textView.isEditable = false
+        
+        textView.text = menuText
+        view.addSubview(textView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        addViews()
     }
 }
 
